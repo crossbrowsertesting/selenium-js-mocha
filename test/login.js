ae8e8ec39f56eeb53bcafeb1d5341f601c69bc09
John Reese <johnr@crossbrowsertesting.com>
2016-10-11 14:38:44 -0500

    Added comments

--- test/login.js
+++ test/login.js
@@ -4,8 +4,8 @@ var assert = require('chai').assert;
 
 var remoteHub = 'http://hub.crossbrowsertesting.com:80/wd/hub';
 
-var username = 'johnr@crossbrowsertesting.com'; //replace with your email address 
-var authkey = 'ub663d2ed3902e28'; //replace with your authkey 
+var username = 'johnr@crossbrowsertesting.com'; // replace with your email address 
+var authkey = 'secretauthkey'; // replace with your authkey 
  
 var caps = {
     name : 'Login',
@@ -25,16 +25,16 @@ describe("Login form", function(){
     // set the timeout for each test in this block to 5 minutes
     this.timeout(5 * 1000 * 60);
 
-
-    // We create the webdriver object on the block level so that it can be accessed by the tests below
+    // Create the webdriver object on the block level so that it can be accessed by the tests below
     var driver = new webdriver.Builder()
         .usingServer(remoteHub)
         .withCapabilities(caps)
         .build();
 
     // This function runs before each "it" block below
-    // We use it to reset the driver back to the correct page.
-    // This is also where we would reset cookies and other browser data we don't want to persist 
+    // Here, it resets the driver back to the correct page.
+    // This is also where we would reset cookies and other browser data we don't want to 
+    // persist between test blocks
     beforeEach(function setupWebdriver(done){
         driver.get("http://crossbrowsertesting.github.io/login-form.html").then( done ) 
     });
@@ -42,18 +42,22 @@ describe("Login form", function(){
     // Each "it" block describes one test scenario
     // The first argument is documentation for the test
     it("rejects bad login credentials", function(done){
+        // Enter username
         var username = driver.findElement(webdriver.By.id("username"));
         username.click()
         .then(function(){username.sendKeys("username"); } )
 
+        // Enter password
         var password = driver.findElement(webdriver.By.id("password"));
         password.click()
         .then(function(){password.sendKeys("password"); } )
 
+        // Click "Login"
         driver.findElement(webdriver.By.css("div.form-actions>button")).click()
+
+        // Check that the login was rejected
         .then(function(){
             return driver.wait(webdriver.until.elementLocated(webdriver.By.css(".alert")), 5000)
-            // return driver.findElement(webdriver.By.css(".alert"));
         })
         .then(function(element){
             return element.getText();
@@ -66,6 +70,7 @@ describe("Login form", function(){
     });
 
     it("accepts good login credentials", function(done){
+
         var username = driver.findElement(webdriver.By.id("username"));
         username.click()
         .then(function(){username.sendKeys("tester@crossbrowsertesting.com"); } )
@@ -75,6 +80,7 @@ describe("Login form", function(){
         .then(function(){password.sendKeys("test123"); } )
 
         driver.findElement(webdriver.By.css("div.form-actions>button")).click()
+
         .then(function(){
             return driver.wait(webdriver.until.elementLocated(webdriver.By.css("#logged-in-message > h2")), 5000)
         })
@@ -87,7 +93,7 @@ describe("Login form", function(){
         .then(done);
     });
 
-    // The after block is run after each "it" block finishes
+    // The after block is run after all "it" blocks finish.
     // If driver.quit() isn't called, the session will remain open until the default 10 minute timeout is reached.
     after(function quitWebdriver(done){
         driver.quit()
